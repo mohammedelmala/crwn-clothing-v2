11673fc68caed17755011f89b1d70a3042760a51
=Mohammed Elmala <=>
2024-07-14 13:08:43 +0400

    create directory page

--- src/App.js
+++ src/App.js
@@ -1,43 +1,39 @@
+import Directory from "./components/directory/directory.component";
 const App = () => {
-  return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    </div>
-  );
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl:
+        "https://images.pexels.com/photos/22670683/pexels-photo-22670683/free-photo-of-man-standing-and-woman-sitting-in-costumes.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl:
+        "https://images.pexels.com/photos/6712035/pexels-photo-6712035.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl:
+        "https://images.pexels.com/photos/5661240/pexels-photo-5661240.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl:
+        "https://images.pexels.com/photos/26933997/pexels-photo-26933997/free-photo-of-two-young-women-wearing-afros-smiling-at-the-camera.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      // "https://images.pexels.com/photos/26888160/pexels-photo-26888160/free-photo-of-portrait-of-woman-sitting-and-smiling-at-park.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl:
+        "https://images.pexels.com/photos/26892135/pexels-photo-26892135/free-photo-of-a-man-wearing-sunglasses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  ];
+  return <Directory categories={categories} />;
 };
 
 export default App;
